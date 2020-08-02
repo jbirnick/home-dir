d3f3392a04456466cb2a087ad7e55e85dfec3f97
Johann Birnick <johann.birnick@hotmail.de>
2020-08-02 18:09:49 +0200

    stuff

--- .sage/init.sage
+++ .sage/init.sage
@@ -4,4 +4,4 @@ latex.blackboard_bold(true)
 
 from sage.misc.viewer import viewer
 viewer.pdf_viewer('bspc rule -a Atril-previewer state=floating -o ; atril --preview')
-viewer.png_viewer('bspc rule -a Eom state=floating -o ; eom -s')
+viewer.png_viewer('bspc rule -a Eom state=floating -o ; eom')
---
+++ .scripts/bspwm-sticky-overlay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+nodes () { bspc query -N -n .above.sticky ; }
+
+toggleHidden () {
+  if [ $# -eq 0 ];
+  then
+    for node in $(nodes); do
+      bspc node ${node} --flag hidden
+    done
+  else
+    for node in $(nodes); do
+      bspc node ${node} --flag hidden=${1}
+    done
+  fi
+}
+
+case $1 in
+  addAndView)
+    bspc node $2 --layer above --state floating --flag sticky
+    eval $1 view
+    toggleHidden off
+    ;;
+  list)echo "$(nodes)" ;;
+  view) toggleHidden off ;;
+  hide) toggleHidden on ;;
+  toggle) toggleHidden
+    ;;
+  *) echo "Unknown argument." ;;
+esac
